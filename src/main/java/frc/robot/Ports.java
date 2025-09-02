7e6942ba5fba3b7cf75405060523735b481a1a47
kasey <xiakasey25@gmail.com>
2025-09-02 17:58:25 -0400

    outtake subsystem

---
+++ src/main/java/frc/robot/Ports.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+public class Ports {
+    public static final class OuttakePorts {
+        public static final int OUTTAKE_MOTOR = 5;
+        public static final int FRONT_BB = 6;
+        public static final int MIDDLE_BB = 7;
+    }
+}
